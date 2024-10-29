c201abd0c6524891c1bd7c617b15a432fc732322
Andreas Worm <aw_github@headissue.com>
2024-10-29 21:20:18 +0100

    Update src/test/java/org/example/AppTest.java

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -30,7 +30,7 @@ public class AppTest {
      * and if both is true, "fizzbuzz"
      */
     @Test
-    public void shouldAnswerWithTrue() {
+    public void alsoSupportsFizzBuzz() {
         assertEquals("1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz", String.join(",", App.fizz(15)));
     }
 }
