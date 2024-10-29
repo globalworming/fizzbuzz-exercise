b7f6c2bdfd5bb7b9081ea7aea703c04b6f3e0bf5
Andreas Worm <aw_github@headissue.com>
2024-10-29 21:36:31 +0100

    Update src/main/java/org/example/App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,7 +12,7 @@ public class App {
             boolean isDivisibleByBoth = i % 3 == 0 & i % 5 == 0;
 
             if (isDivisibleByBoth) {
-                numbers.add("fizzbizz");
+                numbers.add("fizzbuzz");
             } else if (isDivisibleByThreeWithoutRest) {
                 numbers.add("fizz");
             } else if (isDivisibleByFiveWithoutRest) {
