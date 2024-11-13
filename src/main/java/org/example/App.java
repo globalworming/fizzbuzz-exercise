0256c49ee5b7a4d59fd91960ebc704ab5e811141
Wei <181673274+WeiAutomation@users.noreply.github.com>
2024-11-14 09:53:53 +1300

    2 implementation of fizzbuzz

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,8 +2,59 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class App {
+
+    public static final Function<Integer, List<String>> POPULATE_FUNCTION = it -> {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < it; i++) {
+            list.add(i + 1 + "");
+        }
+        return list;
+
+    };
+
+    public static final Function<String, String> FIZZ_FUNCTION = it -> {
+        try {
+            if (Integer.parseInt(it) % 3 == 0) {
+                return "fizz";
+            }
+
+        } catch (NumberFormatException e) {
+            return it;
+
+
+        }
+
+
+        return it;
+    };
+    public static final Function<String, String> BUZZ_FUNCTION = it -> {
+        try {
+            if (Integer.parseInt(it) % 5 == 0) {
+                return "buzz";
+            }
+
+        } catch (NumberFormatException e) {
+
+        }
+
+        return it;
+    };
+    public static final Function<String, String> FIZZBUZZ = it -> {
+        try {
+            if ((Integer.parseInt(it) % 3 == 0) && (Integer.parseInt(it) % 5 == 0)) {
+                return "fizzbuzz";
+            }
+        } catch (NumberFormatException e) {
+        }
+        return it;
+    };
+
+
     public static List<String> fizz(int number) {
         List<String> numbers = new ArrayList<>();
         for (int i = 1; i <= number; i++) {
@@ -26,6 +77,19 @@ public class App {
 
     public static List<String> initList(int i) {
         return null;
+    }
+
+    public static List<String> fizz2(int number) {
+
+        return POPULATE_FUNCTION.apply(number)
+                .stream()
+                .map(FIZZBUZZ)
+                .map(FIZZ_FUNCTION)
+                .map(BUZZ_FUNCTION)
+
+                .collect(Collectors.toList());
+
+
     }
 }
 
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -6,9 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -37,21 +35,17 @@ public class AppTest {
     @Test
     public void alsoSupportsFizzBuzz() {
         assertEquals("1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz", String.join(",", App.fizz(15)));
+        assertEquals("1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz", String.join(",", App.fizz2(15)));
+
     }
 
     // transform input, some elements to fizz
     @Test
     void functionThatFizzes() {
         List<String> input = List.of("1", "2", "3");
-        Function<String, String> fizzFunction = it -> {
-            if (Integer.parseInt(it) % 3 == 0) {
-                return "fizz";
-            }
-            return it;
-        };
 
         Stream<String> mapped = input.stream()
-                .map(fizzFunction);
+                .map(App.FIZZ_FUNCTION);
         String fizzed = mapped.collect(Collectors.joining(","));
         assertEquals("1,2,fizz", fizzed);
     }
@@ -65,15 +59,9 @@ public class AppTest {
     @Test
     void functionThatBuzzes() {
         List<String> input = List.of("1", "5");
-        Function<String, String> buzzFunction = it -> {
-            if (Integer.parseInt(it) % 5 == 0) {
-                return "Buzz";
-            }
-            return it;
-        };
 
         Stream<String> mapped = input.stream()
-                .map(buzzFunction);
+                .map(App.BUZZ_FUNCTION);
         String Buzzed = mapped.collect(Collectors.joining(","));
         assertEquals("1,Buzz", Buzzed);
     }
@@ -81,15 +69,9 @@ public class AppTest {
     @Test
     void functionThatFizzBuzzes() {
         List<String> input = List.of("15", "30");
-        Function<String, String> buzzFunction = it -> {
-            if ((Integer.parseInt(it) % 3 == 0) && (Integer.parseInt(it) % 5 == 0)) {
-                return "FizzBuzz";
-            }
-            return it;
-        };
 
         Stream<String> mapped = input.stream()
-                .map(buzzFunction);
+                .map(App.FIZZBUZZ);
         String FizzBuzzed = mapped.collect(Collectors.joining(","));
         assertEquals("FizzBuzz,FizzBuzz", FizzBuzzed);
     }
@@ -97,18 +79,8 @@ public class AppTest {
     @Test
     void functionThatPopulatesList() {
         int input = 3;
-        Function<Integer, List<String>> PopulateFunction = it -> {
-            List<String> list = new ArrayList<>();
 
-            for (int i = 0; i < it; i++) {
-                list.add(i + 1 + "");
-            }
-            return list;
-
-
-        };
-
-        assertEquals(List.of("1", "2", "3"), PopulateFunction.apply(3));
+        assertEquals(List.of("1", "2", "3"), App.POPULATE_FUNCTION.apply(3));
     }
 }
 
